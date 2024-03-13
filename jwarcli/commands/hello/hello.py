2c4652bde1c68abef6d9f2a2386b5049dd1c5b90
Justin Rabang <justin.rabang@gmail.com>
2024-03-13 14:20:05 +0800

    changed required python version to 3.6 and up

--- jwarcli/commands/hello/hello.py
+++ jwarcli/commands/hello/hello.py
@@ -1,4 +1,7 @@
+from typing import Optional
+
 import typer
+from typing_extensions import Annotated
 
 
 app = typer.Typer()
@@ -7,24 +10,28 @@ app = typer.Typer()
 @app.callback()
 def callback():
     """
-    This will say Hello World
+    This will say Hello World!!!!!!
     """
 
 
-@app.command()
-def hello():
+@app.command(help="Say Hello World!!!")
+def helloworld(
+    dryrun: Annotated[
+        Optional[bool],
+        typer.Option(
+            "--dry-run/--actual-run", help="this will not execute the command"
+        ),
+    ] = None
+):
     """
-    Say Hello
+    Say Hello World
     """
-    typer.echo("Hello")
-
-
-@app.command()
-def world():
-    """
-    Say World
-    """
-    typer.echo("World")
+    if dryrun is None:
+        typer.echo("This is the default, Hello World")
+    elif dryrun:
+        typer.echo("Dry run of Hello World")
+    else:
+        typer.echo("Actual run of Hello World")
 
 
 if __name__ == "__main__":
